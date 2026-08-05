431e53c41e32867924e9e907cbbb4515287d8fe8
youssef-nachar <156086960+youssef-nachar@users.noreply.github.com>
2026-08-05 08:57:07 +0300

    Update Foyer.js

--- Foyer.js
+++ Foyer.js
@@ -878,7 +878,19 @@ function renderExpenses(dataToRender = expenses) {
                 <td style="font-weight: 700; color: #dc2626;">-${formatCurrency(expense.amount)}</td>
                 <td style="text-align: right;">
                     <button class="btn-delete-expense" onclick="deleteExpense(${expense.id})" title="Delete Expense">
-                        <i class="fa-solid fa-trash-can"></i>
+                        <svg width="18" height="18"
+     viewBox="0 0 24 24"
+     fill="none"
+     stroke="currentColor"
+     stroke-width="2"
+     stroke-linecap="round"
+     stroke-linejoin="round">
+    <polyline points="3 6 5 6 21 6"></polyline>
+    <path d="M19 6l-1 14H6L5 6"></path>
+    <path d="M10 11v6"></path>
+    <path d="M14 11v6"></path>
+    <path d="M9 6V4h6v2"></path>
+</svg>
                     </button>
                 </td>
             `;
